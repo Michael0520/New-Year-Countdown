cec13695ad5f36df328cf20f9b47ea8b1b626452
MichaelLuo520 <po2319987@gmail.com>
2021-03-16 00:12:33 +0800

    push all code

--- all.js
+++ all.js
@@ -1,26 +1,46 @@
-const days = document.getElementById("day");
+const days = document.getElementById("days");
 const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const countdown = document.getElementById("countdown");
 
+const year = document.getElementById("year");
+const loading = document.getElementById("loading");
 const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
+// Set background year
+year.innerText = currentYear + 1;
+
+// Update countdown time
 function updateCountdown() {
   const currentTime = new Date();
+
+  // 時間差
   const diff = newYearTime - currentTime;
 
-  // 一年有 365天 總共有 31467458 秒
+  //得到的值是毫秒，所以需要除以1000，變成秒。
   const d = Math.floor(diff / 1000 / 60 / 60 / 24);
   const h = Math.floor(diff / 1000 / 60 / 60) % 24;
   const m = Math.floor(diff / 1000 / 60) % 60;
   const s = Math.floor(diff / 1000) % 60;
-  console.log(d);
-  console.log(h);
-  console.log(m);
-  console.log(s);
+
+  // Add values to DOM
+  // 將取得的時間，更新到HTML上
+  days.innerHTML = d;
+
+  // 如果不是十位數，就在前面加個0，使用三元運算子判斷。
+  hours.innerHTML = h < 10 ? "0" + h : h;
+  minutes.innerHTML = m < 10 ? "0" + m : m;
+  seconds.innerHTML = s < 10 ? "0" + s : s;
 }
 
-// setInterval(updateCountdown, 1000);
+// Show spinner before countdown
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = "flex";
+}, 1000);
+// Run every second
+// 每1000毫秒(1秒)更新一次
+setInterval(updateCountdown, 1000);
